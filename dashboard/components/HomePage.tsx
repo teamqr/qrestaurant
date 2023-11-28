7b69d2f02a6d9a96edaae75524bdc1c8b94e617d
Piotr Kołodziejski <piotr.kolodziejski00@hotmail.com>
2023-11-28 16:10:36 +0100

    Add fetching restaurant data

--- dashboard/components/HomePage.tsx
+++ dashboard/components/HomePage.tsx
@@ -1,11 +1,37 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { signOut, useSession } from "next-auth/react";
+import { jwtDecode } from "jwt-decode";
+
+async function fetchRestaurantData(token: any) {
+  if (token) {
+    const tokenData: any = jwtDecode(token);
+    const restaurantId: number = tokenData.restaurantId;
+    const reqUrl = `http://localhost:8080/api/dashboard/restaurant/${restaurantId}`;
+    const res = await fetch(reqUrl, {
+      headers: { Authorization: "Bearer " + token },
+    });
+    if (res.ok) {
+      const json = await res.json();
+      return json.restaurant;
+    }
+  }
+  return null;
+}
 
 const HomePage = () => {
   const { data: session, status } = useSession({
     required: true,
   });
+
+  const token = session?.user?.userToken;
+
+  const [restaurant, setResturant] = useState(undefined);
+
+  (async () => {
+    const restaurantData = await fetchRestaurantData(token);
+    setResturant(restaurantData?.name);
+  })();
 
   if (status === "loading") {
     return <></>;
@@ -16,6 +42,7 @@ const HomePage = () => {
       <main>
         <h1>Main dashboard</h1>
         <p>Zalogowano jako {session.user?.email}</p>
+        <p>Restauracja: {restaurant} </p>
         <button
           onClick={() => {
             signOut();
