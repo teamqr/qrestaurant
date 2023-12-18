cf25efcae491794bae3dba086624b42e9b80ceb5
Kacper Grabiec <grabiec.kacper01@gmail.com>
2023-12-18 17:59:40 +0100

    fix: make order repository transactional

--- server/qr-app/src/main/java/com/qrestaurant/qrapp/repository/OrderRepository.java
+++ server/qr-app/src/main/java/com/qrestaurant/qrapp/repository/OrderRepository.java
@@ -1,12 +1,14 @@
 package com.qrestaurant.qrapp.repository;
 
 import com.qrestaurant.qrapp.model.entity.Order;
+import jakarta.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface OrderRepository extends CrudRepository<Order, Long> {
     Iterable<Order> getAllByUser_Id(Long userId);
     Optional<Order> findByIdAndUser_Id(Long id, Long userId);
--- server/qr-dashboard/src/main/java/com/qrestaurant/qrdashboard/repository/OrderRepository.java
+++ server/qr-dashboard/src/main/java/com/qrestaurant/qrdashboard/repository/OrderRepository.java
@@ -1,12 +1,14 @@
 package com.qrestaurant.qrdashboard.repository;
 
 import com.qrestaurant.qrdashboard.model.entity.Order;
+import jakarta.transaction.Transactional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
+@Transactional
 public interface OrderRepository extends CrudRepository<Order, Long> {
     Iterable<Order> getAllByRestaurant_Id(Long restaurantId);
     Optional<Order> findByIdAndRestaurant_Id(Long id, Long restaurantId);
