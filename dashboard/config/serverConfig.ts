b1402ce8db0cbde6e0ba4ef30dde8aee4fdf8548
Piotr Kołodziejski <piotr.kolodziejski00@hotmail.com>
2024-01-07 21:06:29 +0100

    Move hardcoded addresses to .env

--- dashboard/config/serverConfig.ts
+++ dashboard/config/serverConfig.ts
@@ -1,1 +1,2 @@
-export const serverUrl = process.env.SERVER_URL ?? "http://localhost:8080";
+export const serverUrl =
+  process.env.NEXT_PUBLIC_SERVER_URL ?? "http://localhost:8080";
--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   experimental: {
     serverActions: {
-      allowedForwardedHosts: ["localhost:3000"],
+      allowedForwardedHosts: ["localhost:3000", "192.168.0.45:3000"],
       allowedOrigins: ["192.168.0.45:3000"],
     },
   },
